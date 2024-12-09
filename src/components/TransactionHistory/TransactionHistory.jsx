901c6c6fba90136ce8a3cfb6890d716479473b84
PKdev <130464415+Pavlo1508@users.noreply.github.com>
2024-12-09 22:50:02 +0100

    Third commit

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,7 +3,7 @@ import TransactionHistoryStyles from './TransactionHistory.module.css';
 const TransactionHistory = ({ type, amount, currency }) => {
   return (
     <>
-      <tr>
+      <tr className={TransactionHistoryStyles.row}>
         <td>{type.charAt(0).toUpperCase() + type.slice(1)}</td>
         <td>{amount}</td>
         <td>{currency}</td>
